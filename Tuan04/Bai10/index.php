4fd7b19c13e1b089e66497b7b3243d250d199eb3
dhViet2004 <22671321.viet@student.iuh.edu.vn>
2026-04-03 22:40:27 +0700

    Done bai 10 phan 2

---
+++ Tuan04/Bai10/index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Test update lần 1 từ file index.php</h1>";
+echo "<p>Ứng dụng PHP đang chạy trên Apache trong Docker.</p>";
+echo "<p>Thời gian hiện tại của hệ thống: " . date('Y-m-d H:i:s') . "</p>";
+phpinfo(); // Hiển thị thông tin chi tiết về PHP
+?>
